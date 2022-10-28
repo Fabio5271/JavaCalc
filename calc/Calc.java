0ea0479669aeec585ace13659690a40db2be8ec1
Fábio <89141452+Fabio5271@users.noreply.github.com>
2022-10-28 15:05:18 -0300

    Added files in the proper directory
    Cleaned up the code and put it into the proper directory structure

---
+++ calc/Calc.java
@@ -0,0 +1,42 @@
+package calc;
+
+import java.util.Scanner;
+
+public class Calc {
+    float res;
+    Scanner input = new Scanner(System.in);
+
+    public Calc(float res){
+        this.res = res;
+    }
+
+    public void Add() {
+        System.out.println("Value to be added: ");
+        res += input.nextFloat();
+    }
+
+    public void Subtract() {
+        System.out.println("Value to be subtracted: ");
+        res -= input.nextFloat();
+    }
+    public void Multiply() {
+        System.out.println("Multiply by: ");
+        res *= input.nextFloat();
+    }
+    public void Divide() {
+        System.out.println("Divide by: ");
+        float div = input.nextFloat();
+        if (div != 0) {
+            res /= div;
+        }
+        else {
+            System.out.println("Error: Division by zero");
+        }
+    }
+    public void Zero() {
+        res = 0;
+    }
+    public void ShowRes() {
+        System.out.println("Result: " + res);
+    }
+}
